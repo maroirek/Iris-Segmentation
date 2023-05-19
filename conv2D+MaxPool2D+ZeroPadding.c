e53d1d90b59cb84db90f1ad9802bec16740a8a77
Maroua REKROUK <82960995+maroirek@users.noreply.github.com>
2023-05-19 21:52:39 +0100

    Zero padding done

--- conv2D+MaxPool2D+ZeroPadding.c
+++ conv2D+MaxPool2D+ZeroPadding.c
@@ -1,90 +1,117 @@
 
 #include <stdio.h>
-#include <math.h>
 
 // image and weights must be given as arrays of dim 1
 
-void conv2D (int *image, int *new_image,  int stride, int img_dim, int kernel, int *weights, int final_dim )
+void Conv2D (int *image, int *new_image,  int stride, int img_dim, int kernel, int *weights, int final_dim, int start)
 {
-    
-    int i;int k;int j; int l=0;
-    int *img = &image[0];
-    int s=0;
-    for (i=0 ; i<final_dim ; i++)
-    {
-      new_image[i]=0;
-      
-      if(img < &image[img_dim-kernel + 1+ l*img_dim]) {img = &image[i*stride+(kernel-1)*s];}
-      else {img = &image[i*stride + (kernel-1)*(s+1)];l++;s++;}
+  int i;int k;int j; int l=0;
+  int *img = &image[0];
+  int s=0;
+  for (i=start ; i<final_dim +start; i++)
+  {
+    new_image[i]=0;
+    if(img < &image[img_dim-kernel + 1+ l*img_dim]) {img = &image[(i-start)*stride+(kernel-1)*s];}
+    else {img = &image[(i-start)*stride + (kernel-1)*(s+1)];l++;s++;}
 
-      for (k=0; k<kernel; k++)
+    for (k=0; k<kernel; k++)
+      {
+        for ( j=0; j<kernel; j++)
         {
-          for ( j=0; j<kernel; j++)
-          {
-          // printf("pixel %d weight %d  \n",img[j+(k*img_dim)] , weights[j + (k*kernel)]);
-          new_image[i] = new_image[i] + img[j+(k*img_dim)]*weights[j + (k*kernel)] ;
-          }
+        // printf("pixel %d weight %d  \n",img[j+(k*img_dim)] , weights[j + (k*kernel)]);
+        new_image[i] = new_image[i] + img[j+(k*img_dim)]*weights[j + (k*kernel)] ;
         }
-      if(img < &image[img_dim-kernel+ 1 + l*img_dim]) {img = &image[i*stride+(kernel-1)*s+1];}
-      else {img = &image[i*stride+(kernel-1)*(s+1)+1];}
-      
-    }
+      }
+    if(img < &image[img_dim-kernel+ 1 + l*img_dim]) {img = &image[(i-start)*stride+(kernel-1)*s+1];}
+    else {img = &image[(i-start)*stride+(kernel-1)*(s+1)+1];}
+  }
 }
 
 void MaxPool2D (int *image, int *new_image,  int stride, int img_dim, int kernel, int final_dim )
 {
+  int i;int k;int j; int l=0;
+  int *img = &image[0];
+  int s=0;
+  for (i=0 ; i<final_dim ; i++)
+  {
+    new_image[i]=0;
     
-    int i;int k;int j; int l=0;
-    int *img = &image[0];
-    int s=0;
-    for (i=0 ; i<final_dim ; i++)
-    {
-      new_image[i]=0;
-      
-      if(img < &image[img_dim-kernel + 1+ l*img_dim]) {img = &image[i*stride+(kernel-1)*s];}
-      else {img = &image[i*stride + (kernel-1)*(s+1)];l++;s++;}
+    if(img < &image[img_dim-kernel + 1+ l*img_dim]) {img = &image[i*stride+(kernel-1)*s];}
+    else {img = &image[i*stride + (kernel-1)*(s+1)];l++;s++;}
 
-      for (k=0; k<kernel; k++)
+    for (k=0; k<kernel; k++)
+      {
+        for ( j=0; j<kernel; j++)
         {
-          for ( j=0; j<kernel; j++)
-          {
-          if(img[j+(k*img_dim)]>new_image[i]){new_image[i] = img[j+(k*img_dim)];}
-          }
+        if(img[j+(k*img_dim)]>new_image[i]){new_image[i] = img[j+(k*img_dim)];}
         }
-      if(img < &image[img_dim-kernel+ 1 + l*img_dim]) {img = &image[i*stride+(kernel-1)*s+1];}
-      else {img = &image[i*stride+(kernel-1)*(s+1)+1];}
-      
-    }
+      }
+    if(img < &image[img_dim-kernel+ 1 + l*img_dim]) {img = &image[i*stride+(kernel-1)*s+1];}
+    else {img = &image[i*stride+(kernel-1)*(s+1)+1];} 
+  }
 }
 
+void ZeroPadding(int *image, int *new_image, int img_dim, int padding)
+{
+ for (int k=0; k<img_dim+2*padding; k++)
+        { if (k<padding)
+          {
+            for (int i=0; i<img_dim+2*padding; i++){new_image[i+k*(img_dim+2*padding)]=0;}
+          }
+          else
+          { if (k<img_dim + padding)
+            {
+            for(int i=0; i<padding; i++){new_image[k*(img_dim+2*padding)+i]=0;}
+            for (int i=padding; i<img_dim+padding; i++){new_image[k*(img_dim+2*padding)+i]=image[i-padding+(k-padding)*img_dim];}
+            for(int i=img_dim+padding;i<img_dim+2*padding; i++){new_image[k*(img_dim+2*padding)+i]=0;}
+            }
+            else
+            {
+            for (int i=0; i<img_dim+2*padding; i++){new_image[i+k*(img_dim+2*padding)]=0;}
+            }
+          
+          }
+        }
+}
+        
+void ConvOneChannel(int *input, int *output, int stride, int img_dim,int nbr_kernels, int * total_weights,int kernel)
+{
+int final_dim;
+int *weight;
+final_dim = ((int)((img_dim - kernel)/stride)+1)*((int)((img_dim - kernel)/stride)+1);
+
+for (int i=0; i<nbr_kernels; i++)
+{
+for (int k =0; k<kernel;k++){weight[k]= total_weights[k+i*(kernel*kernel)];}   // we have the i filter
+int start = i*img_dim*img_dim;
+
+
+}
+
+
+}
+
+// ndiro conv ta3 each channel m3a ga3 l filters, apres we add ga3 l arrays to one array fih ga3 l channels 
 
 
 int main()
 {
-  int image[36] = {1,2,3,4,5,6, 7,8,9,10,11,12, 13,14,15,16,17,18, 19,20,21,22,23,24, 25,26,27,28,29,30, 31,32,33,34,35,36 };
+  int image[36] = {1,2,3,4,5, 6,7,8,9,10, 11,12,13,14,15, 16,17,18,19,20, 21,22,23,24,25};
+
+  // ,26,27,28,29,30, 31,32,33,34,35,36 
+
   int weights[9] = {0,0,0, 0,1,0, 0,0,0};
-  int img_dim = 6;
+  int img_dim = 5;
   int kernel = 2;
   int stride = 1;
   int final_dim;
+  int padding = 1;
 
+  final_dim = ((int)(img_dim + 2*padding))*((int)(img_dim + 2*padding));
   final_dim = ((int)((img_dim - kernel)/stride)+1)*((int)((img_dim - kernel)/stride)+1);
 
-  printf("image final dim is %d \n", final_dim);
-
-  int new_img[final_dim];
-
-
-  // test(image);
-  MaxPool2D(image, new_img, stride, img_dim, kernel,final_dim) ;
-  int i;
-  for(i=0; i<final_dim; i++)
-  {
-   printf("%d \n ",new_img[i]);
- 
-  }
-  
   return 0;
+ 
 }
 
 
