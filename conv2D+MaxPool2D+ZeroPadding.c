09f713e0802083768504489fbff6dc3b6f8ca462
Maroua REKROUK <82960995+maroirek@users.noreply.github.com>
2023-05-21 12:41:25 +0100

    Zero padding many channels and dense done

--- conv2D+MaxPool2D+ZeroPadding.c
+++ conv2D+MaxPool2D+ZeroPadding.c
@@ -52,7 +52,7 @@ void MaxPool2D (int *image, int *new_image,  int stride, int img_dim, int kernel
   }
 }
 
-void ZeroPadding(int *image, int *new_image, int img_dim, int padding)
+void ZeroPadding2D(int *image, int *new_image, int img_dim, int padding)
 {
  for (int k=0; k<img_dim+2*padding; k++)
         { if (k<padding)
@@ -70,28 +70,10 @@ void ZeroPadding(int *image, int *new_image, int img_dim, int padding)
             {
             for (int i=0; i<img_dim+2*padding; i++){new_image[i+k*(img_dim+2*padding)]=0;}
             }
-          
           }
         }
 }
-
-
-void MaxPoolManyChannel(int *input, int *output, int stride,int kernel, int img_dim,int nbr_input_chan,int final_dim)
-{ 
-  // final dim of pooling of one image
-  int input_interm[img_dim*img_dim]; 
-  for (int i=0; i<nbr_input_chan; i++)
-  {  // we have the i filter
-    for (int k =0; k<(img_dim*img_dim);k++){input_interm[k]= input[k+i*(img_dim*img_dim)];} 
-    int start = i*final_dim;
-    MaxPool2D(input_interm,output,stride,img_dim,kernel,final_dim,start);
-  }
-}
-
-
-
-
-        
+      
 void ConvOneChannel(int *input, int *output, int stride, int img_dim,int nbr_kernels, int * total_weights,int kernel,int final_dim)
 {
   //final_dim also ta3 image whda
@@ -104,8 +86,6 @@ void ConvOneChannel(int *input, int *output, int stride, int img_dim,int nbr_ker
     Conv2D(input,output,stride,img_dim,kernel,weight,final_dim,start);
   }
 }
-
-// ndiro conv ta3 each channel m3a ga3 l filters, apres we add ga3 l arrays to one array fih ga3 l channels 
 
 void ConvManyChannel(int *input, int *output, int output_dim, int stride, int img_dim,int nbr_kernels, int * total_weights,int kernel,int final_dim, int nbr_input_chan)
 {  // input fih concatenation ta3 bzf channels
@@ -125,57 +105,96 @@ void ConvManyChannel(int *input, int *output, int output_dim, int stride, int im
       output[j]=output[j]+inter[j];  // must be initilized with zeros out of the function
     }
   }
+}
 
+void MaxPoolManyChannel(int *input, int *output, int stride,int kernel, int img_dim,int nbr_input_chan,int final_dim)
+{ 
+  // final dim of pooling of one image
+  int input_interm[img_dim*img_dim]; 
+  for (int i=0; i<nbr_input_chan; i++)
+  {  // we have the i filter
+    for (int k =0; k<(img_dim*img_dim);k++){input_interm[k]= input[k+i*(img_dim*img_dim)];} 
+    int start = i*final_dim;
+    MaxPool2D(input_interm,output,stride,img_dim,kernel,final_dim,start);
+  }
+}
 
+void ZeroPadding(int *input, int *output, int img_dim, int padding,int nbr_input_chan,int final_dim)
+{
+  // final dim of padding of one image
+  int input_interm[img_dim*img_dim];
+  int output_inter[final_dim];
+  for (int i=0; i<nbr_input_chan; i++)
+  {  // we have the i filter
+    for (int k =0; k<img_dim*img_dim;k++){input_interm[k]= input[k+i*img_dim*img_dim];} 
+    ZeroPadding2D(input_interm,output_inter,img_dim,padding);
+    for (int j =0; j<final_dim;j++){output[j+i*final_dim]= output_inter[j];} 
+  }  
+}
+
+void Dense(int *input, int * weights, int *output, int input_dim, int output_dim ) // input dim since weights are in an array not in a matrix
+{
+  for (int i=0; i<output_dim ; i++)
+  { 
+    output[i]=0;
+    for(int j=0; j<input_dim; j++){output[i]= output[i]+ input[j]*weights[j+i*input_dim];}
+  }
 }
 
 
 
 int main()
 {
-  int image[25] = {1,2,3,4,5, 6,7,8,9,10, 11,12,13,14,15, 16,17,18,19,20, 21,22,23,24,25};
+  // int image[25] = {1,2,3,4,5, 6,7,8,9,10, 11,12,13,14,15, 16,17,18,19,20, 21,22,23,24,25};
 
-  // ,26,27,28,29,30, 31,32,33,34,35,36 
+  // // ,26,27,28,29,30, 31,32,33,34,35,36 
 
-  int weights_layer1[18] = {0,0,0, 0,1,0, 0,0,0, 0,0,0, 0,2,0, 0,0,0}; // 2 filters of kernel 3 
-  int weights_layer2[16] = {1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1}; // 4 filters of kernel 2
-  int img_dim = 5;
-  int kernel_l1 = 3;
-  int kernel_l2 = 2;
-  int stride = 1;
-  int final_dim_padding_l1, final_dim_pooling_l1,final_dim_padding_l2, final_dim_pooling_l2;
-  int padding = 1;
-  int new_image[18]={0};  // 18 = final_dim_pooling*nbr of kernel = 9x2 = 18
-  int nbr_kernels= 4;
+  // int weights_layer1[18] = {0,0,0, 0,1,0, 0,0,0, 0,0,0, 0,2,0, 0,0,0}; // 2 filters of kernel 3 
+  // int weights_layer2[16] = {1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1}; // 4 filters of kernel 2
+  // int img_dim = 5;
+  // int kernel_l1 = 3;
+  // int kernel_l2 = 2;
+  // int stride = 1;
+  // int final_dim_padding_l1, final_dim_pooling_l1,final_dim_padding_l2, final_dim_pooling_l2;
+  // int padding = 1;
+  // int new_image[18]={0};  // 18 = final_dim_pooling*nbr of kernel = 9x2 = 18
+  // int nbr_kernels= 4;
   
-  int dim_output = final_dim_pooling_l2*nbr_kernels;
+  // int dim_output = final_dim_pooling_l2*nbr_kernels;
 
-  // final dim is total dim we calculate it once wkhlas
-  final_dim_padding_l1 = ((int)(img_dim + 2*padding))*((int)(img_dim + 2*padding));
-  final_dim_pooling_l1 = ((int)((img_dim - kernel_l1)/stride)+1)*((int)((img_dim - kernel_l1)/stride)+1);
+  // // final dim is total dim we calculate it once wkhlas
+  // final_dim_padding_l1 = ((int)(img_dim + 2*padding))*((int)(img_dim + 2*padding));
+  // final_dim_pooling_l1 = ((int)((img_dim - kernel_l1)/stride)+1)*((int)((img_dim - kernel_l1)/stride)+1);
 
-  // printf("%d \n", final_dim_pooling_l1 );
-  final_dim_padding_l2 = ((int)(img_dim + 2*padding))*((int)(img_dim + 2*padding));
-  final_dim_pooling_l2 = ((int)((final_dim_pooling_l1 - kernel_l2)/stride)+1)*((int)((final_dim_pooling_l1 - kernel_l2)/stride)+1);
-  // printf("%d \n", final_dim_pooling_l2 ); // false
+  // // printf("%d \n", final_dim_pooling_l1 );
+  // final_dim_padding_l2 = ((int)(img_dim + 2*padding))*((int)(img_dim + 2*padding));
+  // final_dim_pooling_l2 = ((int)((final_dim_pooling_l1 - kernel_l2)/stride)+1)*((int)((final_dim_pooling_l1 - kernel_l2)/stride)+1);
+  // // printf("%d \n", final_dim_pooling_l2 ); // false
 
-  int output[16]={0};  // 16 = 4 (image l9dima apres pooling m3a filtre 2x2) x 4 (nbr de kernels)
-  int output_dim=16;
-  int max_pool_output[4];
-  // // Conv2D(image, new_image, stride, img_dim, kernel, weights, final_dim_pooling,9); done
-  ConvOneChannel(image, new_image, stride, img_dim,2 ,weights_layer1, kernel_l1, final_dim_pooling_l1); // done new image 3x3x2
-  ConvManyChannel(new_image, output, output_dim, stride, 3, nbr_kernels, weights_layer2, kernel_l2,4, 2); // done
+  // int output[16]={0};  // 16 = 4 (image l9dima apres pooling m3a filtre 2x2) x 4 (nbr de kernels)
+  // int output_dim=16;
+  // int max_pool_output[4];
+  // // // Conv2D(image, new_image, stride, img_dim, kernel, weights, final_dim_pooling,9); done
+  // ConvOneChannel(image, new_image, stride, img_dim,2 ,weights_layer1, kernel_l1, final_dim_pooling_l1); // done new image 3x3x2
+  // ConvManyChannel(new_image, output, output_dim, stride, 3, nbr_kernels, weights_layer2, kernel_l2,4, 2); // done
   
 
-  // MaxPool2D (output, max_pool_output,  stride, 2, 2, 1, 4); // done
-  MaxPoolManyChannel(output, max_pool_output, stride,2, 2,4,1); //done
+  // // MaxPool2D (output, max_pool_output,  stride, 2, 2, 1, 4); // done
+  // MaxPoolManyChannel(output, max_pool_output, stride,2, 2,4,1); //done
+
+  // int zeroo[36]={0};
+  // // ZeroPadding2D(max_pool_output, zeroo, 2, 1);
+  // ZeroPadding(max_pool_output, zeroo, 1, 1, 4,9);
+
+  // // for(int i=0; i<4;i++){printf("%d",max_pool_output[i]);}
+
+  int in[3]={1,2,3};
+  int w[12]={1,1,1,2,2,2,3,3,3,4,4,4};
+  int out[4];
+  Dense(in, w, out, 3, 4);
 
 
-
-
-  
-  // for(int i=0; i<18;i++){printf("%d",new_image[i]);}
-  for(int i=0; i<4;i++){printf("%d \n",max_pool_output[i]);}
+  for(int i=0; i<4;i++){printf("%d",out[i]);}
   return 0;
  
 }
